f45d7ccfac12fed9d2e26e379e32c59c11ada432
Pritam <manoharipritam@gmail.com>
2024-10-30 19:35:14 +0530

    add clerk

---
+++ components/Header.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Header = ({ children }: HeaderProps) => {
+    return (
+        <div className='header'>
+            <Link href='/' className=' md:flex-1'>
+                <Image
+                    src={"/assets/icons/logo.svg"}
+                    alt="logo with name"
+                    className='hidden md:block'
+                    width={120}
+                    height={32}
+                />
+                <Image
+                    src={"/assets/icons/logo-icon.svg"}
+                    alt="logo "
+                    className='block md:hidden'
+                    width={32}
+                    height={32}
+                />
+            </Link>
+
+            {children}
+        </div>
+    )
+}
+
+export default Header
